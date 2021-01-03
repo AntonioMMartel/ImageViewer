f89fd9382a6c68db7f7ab57b6dfe4fa8c2f5f9bc
Usuario <Usuario@DESKTOP-DGNAOIQ>
2021-01-03 19:31:06 +0000

    Image viewer ver1
    Estamos con el Mock para empezar e ir viendo que necesitamos.
    
    Se implementan los comandos  que ni se acercan al patron command para ir pasando de imagen a imagen

--- src/imageviewer/ImageViewer.java
+++ src/imageviewer/ImageViewer.java
@@ -12,6 +12,7 @@ import imageviewer.Models.Image;
 import imageviewer.View.ImageDisplay;
 import imageviewer.View.ImageListLoader;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author Antonio Miguel Martel
@@ -20,11 +21,27 @@ public class ImageViewer {
 
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
         ImageDisplay imageDisplay = new MockImageDisplay();
         ImageListLoader imageListLoader = new MockImageLoader();
         List<Image> lista = imageListLoader.load();
         imageDisplay.display(lista.get(0));
-        
+        int index = 0;
+        while(true){
+            String input = scanner.next();
+            if( input.equals("q")) break;
+            if(input.equals("d")){
+                index++;
+                if(index >= lista.size()) index=0;
+                imageDisplay.display(lista.get(index));
+            }
+            if(input.equals("a")){
+                index--;
+                if(index < 0) index=lista.size() - 1;
+                imageDisplay.display(lista.get(index));
+            }
+        }
         
     }
 
